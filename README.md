660cbb8411c96fa5b86e1f38e3d66189ba1e8521
appleskiller@163.com <appleskiller@163.com>
2017-11-18 21:58:39 +0800

    add karma-saucelabs-launcher config

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# reflection
-Reflection for Typescript
+# reflector
+Reflector for Typescript
+
+
+## Testing
+
+### Using Mocha
+```
+$ npm test
+```
+
+### Using karma-sauceLabs-launcher
+For run the tests across many browsers and platforms on Sauce Labs, you must make file 'karma.sauceLabs.json' in the project:
+```
+{
+	"testName": "your test name",
+	"username": "your SauceLabs username",
+	"accessKey": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
+```
+The customLaunchers object configures individual browsers in file 'karma.conf.js', and the sauceLabs reporter allows your tests results to be properly displayed on https://saucelabs.com
+
+[all browser/platform combos](https://saucelabs.com/platforms)
+
+[karma-sauce-launcher](https://www.npmjs.com/package/karma-sauce-launcher)
--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration
 // Generated on Mon Nov 13 2017 21:56:03 GMT+0800 (中国标准时间)
-const { CheckerPlugin } = require('awesome-typescript-loader');
+
+var karma_sauceLabs = require("./karma.sauceLabs.json");
 
 module.exports = function(config) {
   config.set({
@@ -15,13 +16,13 @@ module.exports = function(config) {
 
     client: {
       mocha: {
-        opts: "mocha.opts"
+        opts: "test/mocha.opts"
       }
     },
 
     // list of files / patterns to load in the browser
     files: [
-      "test/**/*.spec.ts"
+      'test/index.js',
     ],
 
 
@@ -29,10 +30,10 @@ module.exports = function(config) {
     exclude: [
     ],
 
-
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'test/index.js': ["webpack"],
       'test/**/*.ts': ["webpack"]
     },
     webpack: {
@@ -49,37 +50,37 @@ module.exports = function(config) {
           }
         ],
       },
-      plugins: [
-        new CheckerPlugin()
-      ]
+    },
+    // from file karma.sauceLabs.json
+    sauceLabs: karma_sauceLabs,
+    customLaunchers: {
+      sl_chrome: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Windows 7',
+        version: '35'
+      },
     },
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
+    reporters: ['progress', 'saucelabs'],
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+    // browsers: ['sl_chrome'],
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
